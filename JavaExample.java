35c829460a12e5821a6759ba32232115e4f94f24
Keerthana-P0310 <111142502+Keerthana-P0310@users.noreply.github.com>
2022-08-12 22:22:14 +0530

    Add files via upload

---
+++ JavaExample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class JavaExample {
+   public static void main(String[] args) {
+      ArrayList<Integer> al2 = new ArrayList<Integer>();
+	  
+	    al2.add(1);
+        al2.add(34);
+        al2.add(99);
+        al2.add(99);
+        al2.add(78);
+        System.out.println("Elements of ArrayList of Integer Type: "+al2);
+    }
+}
